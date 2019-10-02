55ef5e0e70b692747679e67860f221f1d7a854b3
Travis Statham <testats@gmail.com>
2019-10-02 19:49:03 -0400

    Added method to find min length word and tests

--- trowe/MaxLengthWord.java
+++ trowe/MaxLengthWord.java
@@ -14,10 +14,8 @@ class MaxLengthWord {
 	 * that Java's String class has a .length() method, I'll be using that in test asserts
 	 */
 	public static String maxLength(String sentence) {
-		//remove all punctuation using regex, then split words based on spaces.
 		
-		sentence = sentence.replaceAll("\\p{P}", "");
-		String[] words = sentence.split(" ");
+		String[] words = splitUpSentence(sentence);
 		int maxLength = 0;
 		String maxLengthWord = "";
 		for(String word: words) {
@@ -31,16 +29,36 @@ class MaxLengthWord {
 		System.out.println("Max Length word length is: "+ maxLength);
 		return maxLengthWord;
 	}
+	//remove all punctuation using regex, then split words based on spaces.
+	
+	private static String[] splitUpSentence(String sentence) {
+		return sentence.replaceAll("\\p{P}", "").split("\\s+");
+		
+	}
+	public static String minLength(String sentence) {
+		String[] words = splitUpSentence(sentence);
+		String minLengthWord = "";
+		int minLength = Integer.MAX_VALUE;
+		for(String word : words) {
+			if(word.length() < minLength) {
+				minLengthWord = word;
+				minLength = word.length();
+			}
+		}
+		System.out.println("The min length word is: " + minLengthWord);
+		System.out.println("Min Length word length is: "+ minLength);
+		return minLengthWord;
+	}
 	
 	@Test
-	public void testEmptySentence() {
+	public void testMaxEmptySentence() {
 		String testWord = "";
 		String result = maxLength(testWord);
 		Assert.assertEquals(result, "");
 	}
 	
 	@Test
-	public void testPeriod() {
+	public void testMaxPeriod() {
 		String testWord = "The quick brown. fox jumped over the lazy dog.";
 		String result = maxLength(testWord);
 		Assert.assertEquals(result, "jumped");
@@ -48,7 +66,7 @@ class MaxLengthWord {
 	}
 	
 	@Test
-	public void testMultipleSpaces() {
+	public void testMaxMultipleSpaces() {
 		String testWord = "Many     spaces! between these words.";
 		String result = maxLength(testWord);
 		Assert.assertEquals(result, "between");
@@ -56,9 +74,50 @@ class MaxLengthWord {
 	}
 	
 	@Test
-	public void testOneWord() {
+	public void testMaxOneWord() {
 		String testWord = "Pneumonoultramicroscopicsilicovolcanoconiosis";
-		Assert.assertEquals(maxLength(testWord), "Pneumonoultramicroscopicsilicovolcanoconiosis");
-		Assert.assertEquals(maxLength(testWord).length(), 45);
+		String result = maxLength(testWord);
+		Assert.assertEquals(result, "Pneumonoultramicroscopicsilicovolcanoconiosis");
+		Assert.assertEquals(result.length(), 45);
 	}
+	@Test
+	public void testMinEmptySentence() {
+		String testWord = "";
+		String result = minLength(testWord);
+		Assert.assertEquals(result, "");
+		Assert.assertEquals(result.length(), 0);
+	}
+	
+	@Test
+	public void testMinPeriod() {
+		String testWord = "The quick brown. fox jumped over the lazy dog.";
+		String result = minLength(testWord);
+		Assert.assertEquals(result, "The");
+		Assert.assertEquals(result.length(), 3);
+	}
+	
+	@Test
+	public void testMinMultipleSpaces() {
+		String testWord = "Many    spaces   between  these words.";
+		String result = minLength(testWord);
+		Assert.assertEquals(result, "Many");
+		Assert.assertEquals(result.length(), 4);
+	}
+	
+	@Test
+	public void testMinMultiplePunct() {
+		String testWord = "Why% is the sky! blue?. Not!@#$%^&*() sure about that.";
+		String result = minLength(testWord);
+		Assert.assertEquals(result, "is");
+		Assert.assertEquals(result.length(), 2);
+	}
+	
+	@Test
+	public void testMinOneWord() {
+		String testWord = "Pneumonoultramicroscopicsilicovolcanoconiosis";
+		String result = minLength(testWord);
+		Assert.assertEquals(result, "Pneumonoultramicroscopicsilicovolcanoconiosis");
+		Assert.assertEquals(result.length(), 45);
+	}
+
 }
